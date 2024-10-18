3b9c918dc56c4933efe5c4d8de1e24862980add2
Kehdinga Raymond <kehdingar@gmail.com>
2024-10-18 14:34:05 +0100

    feat: add getting data from api for departure

---
+++ src/components/Departure.js
@@ -0,0 +1,268 @@
+import multiLogo from '../images/multi.png';
+import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
+
+  const convertMinutesToHours = (totalMinutes) => {
+    const hours = Math.floor(totalMinutes / 60);
+    const minutes = totalMinutes % 60; 
+    return `${hours} hrs ${minutes} mins`;
+  };
+
+  const convertTo12HourFormat = (timeString) => {
+    const date = new Date(timeString);
+    let hours = date.getHours();
+    const minutes = date.getMinutes();
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12 || 12;
+    const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+    return `${hours}:${formattedMinutes} ${ampm}`;
+  };
+
+  
+  const Departure = () => {
+      const location = useLocation();
+      const navigate = useNavigate();
+      const flights = location.state?.flights || []; 
+      const itineraries = location.state?.itineraries || []; 
+      const airlineLogos = location.state?.airlineLogos || [];  
+      console.log("FLIGHTS",flights)
+
+      const handleSelectDepartureFlight = (flight) => {
+  
+        navigate('/return', {
+          state: {
+            selectedDepartureFlight: [flight],
+            itineraries: itineraries,
+            airlineLogos: airlineLogos,
+          }
+        });        
+    
+      };
+
+    const [openAccordionId, setOpenAccordionId] = useState(null);
+  
+    const toggleAccordion = (id) => {
+        setOpenAccordionId((prevId) => (prevId === id ? null : id));
+      };
+    
+
+  return(
+<div>
+  {itineraries.length > 0 || flights.status === false ? (
+    <p className="max-w-2xl mx-auto pt-4 px-4 font-bold text-left">Best Departing Flights</p>
+  ) : (
+    ""
+  )}
+
+  {itineraries.length > 0 || flights.status === false ? (
+    itineraries.map((flight) => (
+      <div key={flight.id} className="max-w-2xl mx-auto p-4">
+        <div
+          className="flex flex-col sm:flex-row justify-between items-center border rounded-lg p-4 cursor-pointer hover:bg-gray-100"
+          onClick={() => toggleAccordion(flight.id)}
+        >
+          {/* Airline Logo */}
+          <div className="flex items-center mb-4 sm:mb-0">
+            {flight.legs[0].stopCount > 1 ? (
+              <img
+                src={multiLogo}
+                alt="Airline Logo"
+                className="w-10 h-10 mr-4"
+              />
+            ) : (
+              <img
+                src={
+                  flight.legs[0].carriers.marketing.length > 1
+                    ? airlineLogos[flight.legs[0].carriers.marketing[1].id].logoUrl
+                    : airlineLogos[flight.legs[0].carriers.marketing[0].id].logoUrl
+                }
+                alt={flight.legs[0].carriers.marketing.name}
+                className="w-10 h-10 mr-4"
+              />
+            )}
+
+            {openAccordionId === flight.id ? (
+              <div className="text-left">
+                <p className="font-bold">
+                  Departure .{" "}
+                  {new Date(flight.legs[0].departure).toLocaleDateString(
+                    "en-US",
+                    { weekday: "short", month: "short", day: "numeric" }
+                  )}
+                </p>
+              </div>
+            ) : (
+              <div className="text-left">
+                <p className="font-bold">
+                  {convertTo12HourFormat(flight.legs[0].departure)} -{" "}
+                  {convertTo12HourFormat(flight.legs[0].arrival)}
+                </p>
+                <p className="text-sm text-gray-500">
+                  {flight.legs[0].stopCount > 1 &&
+                  flight.legs[0].carriers.marketing.length > 1 ? (
+                    flight.legs[0].carriers.marketing.map((airline, index) => (
+                      <span key={airline.id}>
+                        {airline.name}
+                        {index <
+                          flight.legs[0].carriers.marketing.length - 1 &&
+                          ", "}
+                      </span>
+                    ))
+                  ) : flight.legs[0].carriers.marketing.length > 1 ? (
+                    <span>{flight.legs[0].carriers.marketing[1].name}</span>
+                  ) : (
+                    <span>{flight.legs[0].carriers.marketing[0].name}</span>
+                  )}
+                </p>
+              </div>
+            )}
+          </div>
+
+          {/* Total Time and Stops (hidden when open) */}
+          {openAccordionId !== flight.id && (
+            <>
+              <div className="text-left mb-4 sm:mb-0">
+                <p className="font-bold">
+                  {convertMinutesToHours(
+                    flight.legs[0].segments.reduce(
+                      (accumulator, segment) =>
+                        accumulator + segment.durationInMinutes,
+                      0
+                    )
+                  )}
+                </p>
+                <p className="text-sm text-gray-500">
+                  {flight.legs[0].origin.displayCode} -{" "}
+                  {flight.legs[0].destination.displayCode}
+                </p>
+              </div>
+
+              <div className="text-left mb-4 sm:mb-0">
+                <p className="font-bold">
+                  {flight.legs[0].stopCount < 2 ? (
+                    <span>{flight.legs[0].stopCount} stop</span>
+                  ) : (
+                    <span>{flight.legs[0].stopCount} stops</span>
+                  )}
+                </p>
+                <p className="text-sm text-gray-500">
+                  {flight.legs[0].segments.slice(0, -1).map((stop, index) => (
+                    <span key={index}>
+                      {flight.legs[0].stopCount === 1 ? (
+                        <span>
+                          {convertMinutesToHours(stop.durationInMinutes)}{" "}
+                          {stop.destination.displayCode}
+                        </span>
+                      ) : (
+                        stop.destination.displayCode
+                      )}
+                      {index < flight.legs[0].segments.length - 2 && ", "}
+                    </span>
+                  ))}
+                </p>
+              </div>
+            </>
+          )}
+
+          {openAccordionId === flight.id ? (
+            <button
+              onClick={() => handleSelectDepartureFlight(flight)}
+              className="mt-2 bg-green-500 text-white p-2 rounded mx-2"
+            >
+              Select Flight
+            </button>
+          ) : (
+            ""
+          )}
+
+          <div className="text-left">
+            <p className="font-bold">{flight.price.formatted}</p>
+            <p className="text-sm text-gray-500">round trip</p>
+          </div>
+
+          <div className="ml-4">
+            <svg
+              className={`w-6 h-6 transform transition-transform ${
+                openAccordionId === flight.id ? "rotate-180" : ""
+              }`}
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M19 9l-7 7-7-7"
+              />
+            </svg>
+          </div>
+        </div>
+
+        {/* Accordion Content */}
+        {openAccordionId === flight.id && (
+<div className="border rounded-lg p-4 shadow-lg bg-white sm:flex-col sm:items-center">
+  {flight.legs[0].segments.map((segment, index) => (
+    <div key={index} className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4 mb-10">
+      <div className="flex flex-col sm:flex-row items-center">
+
+        {/* Logo Container */}
+        {flight.legs[0].stopCount > 1 && (
+          <div className="flex items-center mb-4 sm:mb-0">
+            <img
+              src={airlineLogos[segment.marketingCarrier.id].logoUrl}
+              alt={flight.legs[0].carriers.marketing.name}
+              className="w-8 h-8"
+            />
+          </div>
+        )}
+
+        <div className="flex flex-row sm:flex-col items-center justify-center h-full mb-4 sm:mb-0 sm:mr-4">
+
+          <div className="flex flex-col items-center mb-2 hidden sm:block ">
+            <div className="w-2 h-2 bg-blue-500 rounded-full"></div>
+          </div>
+
+          <div className="flex-grow border-l-2 border-dashed border-gray-400 hidden sm:block"></div>
+
+          <div className="flex flex-col items-center mt-2 hidden sm:block">
+            <div className="w-2 h-2 bg-blue-500 rounded-full"></div>
+          </div>
+        </div>
+
+        <div className="flex flex-col justify-between h-full ml-2 sm:ml-4">
+          <p className="text-sm text-left font-bold">
+            {convertTo12HourFormat(segment.departure)} .{" "}
+            {segment.origin.name} Airport ({segment.origin.displayCode})
+          </p>
+
+          <p className="text-sm text-left my-2 sm:my-5">
+            Travel Time: {convertMinutesToHours(segment.durationInMinutes)}
+          </p>
+
+          <p className="text-sm text-left font-bold">
+            {convertTo12HourFormat(segment.arrival)} .{" "}
+            {segment.destination.name} Airport (
+            {segment.destination.displayCode})
+          </p>
+        </div>
+      </div>
+    </div>
+  ))}
+</div>
+
+        )}
+      </div>
+    ))
+  ) : (
+    <p>No flights found.</p>
+  )}
+</div>
+
+  )
+};
+
+export default Departure;
---
+++ src/components/FlightSearch.js
@@ -0,0 +1,339 @@
+import React, { useState,useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate, useLocation } from 'react-router-dom';
+import Departure from './Departure';
+
+const FlightSearch = () => {
+  const navigate = useNavigate();
+  const [originSkyId, setOriginSkyId] = useState('ABV');
+  const [destinationSkyId, setDestinationSkyId] = useState('DXB');
+  const [originEntityId, setOriginEntityId] = useState('128668198');
+  const [destinationEntityId, setDestinationEntityId] = useState('95673506');
+  const [date, setDate] = useState();
+  const [returnDate, setReturnDate] = useState();
+  const [cabinClass, setCabinClass] = useState('economy');
+  const [adults, setAdults] = useState(1);
+  const [children, setChildren] = useState(0);
+  const [infants, setInfants] = useState(0);
+  const [sortBy, setSortBy] = useState('best');
+  const [originSuggestions, setOriginSuggestions] = useState([]);
+  const [destinationSuggestions, setDestinationSuggestions] = useState([]); 
+  const [showOriginSuggestions, setShowOriginSuggestions] = useState(false);
+  const [showDestinationSuggestions, setShowDestinationSuggestions] = useState(false);
+  const [loading, setLoading] = useState(false); 
+  const [dataError, setDataError] = useState(null); 
+  const location = useLocation(); 
+
+
+  const fetchOriginCityDetails = async (originCity) => {
+    try {
+      const response = await axios.get(`https://sky-scrapper.p.rapidapi.com/api/v1/flights/searchAirport`, {
+        params: {
+          query: originCity,
+          locale: 'en-US'
+        },
+        headers: {
+          'X-RapidAPI-Key': '9d957b647dmsh61b8df5bedf7854p16d406jsn69ff88bf3b76',
+          'X-RapidAPI-Host': 'sky-scrapper.p.rapidapi.com',
+        },          
+      });
+      setOriginSkyId(response.data.originSkyId);
+      setOriginEntityId(response.data.originEntityId);
+      setOriginSuggestions(response.data.data || []);
+      setShowOriginSuggestions(true);
+    } catch (error) {
+      console.error("Error fetching Sky IDs:", error);
+    }
+  };
+
+  const handleOriginSelection = (suggestion) => {
+    setOriginSkyId(suggestion.skyId); 
+    setOriginEntityId(suggestion.entityId);
+    setShowOriginSuggestions(false); 
+  };  
+  
+  const handleDestinationSelection = (suggestion) => {
+    console.log("id",suggestion)
+    setDestinationSkyId(suggestion.skyId);
+    setDestinationEntityId(suggestion.entityId); 
+    setShowDestinationSuggestions(false); 
+  };
+
+  const fetchDestinationCityDetails = async (destinationCity) => {
+    try {
+      const response = await axios.get(`https://sky-scrapper.p.rapidapi.com/api/v1/flights/searchAirport`, {
+        params: {
+          query: destinationCity,
+          locale: 'en-US'
+        },
+        headers: {
+          'X-RapidAPI-Key': '9d957b647dmsh61b8df5bedf7854p16d406jsn69ff88bf3b76',
+          'X-RapidAPI-Host': 'sky-scrapper.p.rapidapi.com',
+        },        
+      });
+
+      setDestinationSkyId(response.data.destinationSkyId);
+      setDestinationEntityId(response.data.destinationEntityId);
+      setDestinationSuggestions(response.data.data || []); 
+      setShowDestinationSuggestions(true);
+    } catch (error) {
+      console.error("Error fetching Sky IDs:", error);
+    }
+  };
+
+  // Hide dropdown list for origins and destinations when clicked outside the list
+  const handleGeneralclick = () =>{
+    setShowOriginSuggestions(false)
+    setShowDestinationSuggestions(false)
+  }
+
+  useEffect(() => {
+    // Attach the event listener to the window
+    window.addEventListener('click', handleGeneralclick);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('click', handleGeneralclick);
+    };
+  }, []);  
+
+
+  const fetchFlights = async () => {
+    setLoading(true);
+    setDataError(null);
+    try {
+      const response = await axios.get('https://sky-scrapper.p.rapidapi.com/api/v2/flights/searchFlightsComplete', {
+        params: {
+          originSkyId: originSkyId,
+          destinationSkyId: destinationSkyId,
+          originEntityId: originEntityId,
+          destinationEntityId: destinationEntityId,
+          date: date,
+          returnDate: returnDate,
+          cabinClass: cabinClass,
+          adults: adults,
+          childrens: children,
+          infants: infants,
+          sortBy: sortBy,
+          currency: 'USD',
+          market: 'en-US',
+          countryCode: 'US',
+        },
+        headers: {
+          'X-RapidAPI-Key': '9d957b647dmsh61b8df5bedf7854p16d406jsn69ff88bf3b76',
+          'X-RapidAPI-Host': 'sky-scrapper.p.rapidapi.com',
+        },
+      });
+
+      if (response.data.status){
+        const logoMap = {};
+        const logos = response.data.data.filterStats.carriers;
+        logos.forEach(logo => {
+          logoMap[logo.id] = logo;
+        });
+
+        const flightsData = response.data;
+        const itinerariesData = response.data.data.itineraries;
+    
+        // Navigate to departure page after fetching flights
+        navigate('/departure', {
+          state: {
+            flights: flightsData,
+            itineraries: itinerariesData,
+            airlineLogos: logoMap,
+          }
+        });
+      }else{
+        console.log(response.data.message)
+        setDataError(...response.data.message)
+      }
+    } catch (error) {
+      console.error(error);
+    }finally {
+      setLoading(false);
+    }
+  };
+  return (
+<div className="p-4 max-w-full bg-white shadow-md rounded-lg lg:px-40 md:px-40">
+  <h1 className="text-2xl font-bold mb-6 text-center">Flight Search</h1>
+
+  <div className="relative mb-4">
+    <label className="block text-sm font-medium mb-1 text-gray-600" htmlFor="originSkyId">Origin Sky ID:</label>
+    <input 
+      id="originSkyId"
+      type="text" 
+      value={originSkyId} 
+      onChange={(e) => fetchOriginCityDetails(e.target.value)} 
+      placeholder="Origin Sky ID" 
+      className="border rounded-md p-3 w-full" 
+    />
+    {showOriginSuggestions && (
+      <ul className="absolute border bg-white w-full rounded-md z-10 max-h-40 overflow-y-auto shadow-lg">
+        {originSuggestions.map((suggestion, index) => (
+          <li 
+            key={index} 
+            onClick={() => handleOriginSelection(suggestion)} 
+            className="cursor-pointer hover:bg-gray-200 p-2"
+          >
+            {suggestion.presentation.suggestionTitle}
+          </li>
+        ))}
+      </ul>
+    )}
+  </div>
+
+  <div className="relative mb-4">
+    <label className="block text-sm font-medium mb-1 text-gray-600" htmlFor="destinationSkyId">Destination Sky ID:</label>
+    <input 
+      id="destinationSkyId"
+      type="text" 
+      value={destinationSkyId} 
+      onChange={(e) => fetchDestinationCityDetails(e.target.value)} 
+      placeholder="Destination Sky ID" 
+      className="border rounded-md p-3 w-full" 
+    />
+    {showDestinationSuggestions && (
+      <ul className="absolute border bg-white w-full rounded-md z-10 max-h-40 overflow-y-auto shadow-lg">
+        {destinationSuggestions.map((suggestion, index) => (
+          <li 
+            key={index} 
+            onClick={() => handleDestinationSelection(suggestion)} 
+            className="cursor-pointer hover:bg-gray-200 p-2"
+          >
+            {suggestion.presentation.suggestionTitle}
+          </li>
+        ))}
+      </ul>
+    )}
+  </div>
+
+  <div className="flex flex-col sm:flex-row sm:space-x-4 mb-4">
+    <div className="w-full mb-4 sm:mb-0">
+      <label className="block text-sm font-medium mb-1 text-gray-600" htmlFor="departureDate">Departure Date:</label>
+      <input 
+        id="departureDate"
+        type="date" 
+        value={date} 
+        onChange={(e) => setDate(e.target.value)} 
+        className="border rounded-md p-3 w-full" 
+      />
+    </div>
+    <div className="w-full">
+      <label className="block text-sm font-medium mb-1 text-gray-600" htmlFor="returnDate">Return Date: (optional)</label>
+      <input 
+        id="returnDate"
+        type="date" 
+        value={returnDate} 
+        onChange={(e) => setReturnDate(e.target.value)} 
+        className="border rounded-md p-3 w-full" 
+      />
+    </div>
+  </div>
+
+  <div className="mb-4">
+    <label className="block text-sm font-medium mb-1 text-gray-600" htmlFor="cabinClass">Cabin Class: (optional)</label>
+    <select 
+      id="cabinClass"
+      value={cabinClass} 
+      onChange={(e) => setCabinClass(e.target.value)} 
+      className="border rounded-md p-3 w-full"
+    >
+      <option value="economy">Economy</option>
+      <option value="premium_economy">Premium Economy</option>
+      <option value="business">Business</option>
+      <option value="first">First</option>
+    </select>
+  </div>
+
+  <div className="mb-4">
+    <label className="block text-sm font-medium mb-1 text-gray-600" htmlFor="sortBy">Sort By: (optional)</label>
+    <select 
+      id="sortBy"
+      value={sortBy} 
+      onChange={(e) => setSortBy(e.target.value)} 
+      className="border rounded-md p-3 w-full"
+    >
+      <option value="best">Best</option>
+      <option value="price_high">Cheapest</option>
+      <option value="fastest">Fastest</option>
+      <option value="outbound_take_off_time">Outbound Take Off Time</option>
+      <option value="outbound_landing_time">Outbound Landing Time</option>
+      <option value="return_take_off_time">Return Take Off Time</option>
+      <option value="return_landing_time">Return Landing Time</option>
+    </select> 
+  </div>
+
+  <div className="flex flex-col sm:flex-row sm:space-x-4 mb-4">
+    <div className="w-full mb-4 sm:mb-0">
+      <label className="block text-sm font-medium mb-1 text-gray-600" htmlFor="adults">Adults (12+ years): (optional)</label>
+      <input 
+        id="adults"
+        type="number" 
+        min="1" 
+        max="12" 
+        value={adults} 
+        onChange={(e) => setAdults(e.target.value)} 
+        placeholder="" 
+        className="border rounded-md p-3 w-full" 
+      />
+    </div>
+    
+    <div className="w-full">
+      <label className="block text-sm font-medium mb-1 text-gray-600" htmlFor="children">Children (2-12 years): (optional)</label>
+      <input 
+        id="children"
+        type="number" 
+        min="0" 
+        max="10" 
+        value={children} 
+        onChange={(e) => setChildren(e.target.value)} 
+        placeholder="" 
+        className="border rounded-md p-3 w-full" 
+      />
+    </div>
+  </div>
+
+  <div className="">
+    <label className="block text-sm font-medium mb-1 text-gray-600" htmlFor="infants">Infants (Under 2 years): (optional)</label>
+    <input 
+      id="infants"
+      type="number" 
+      min="0" 
+      max="10" 
+      value={infants} 
+      onChange={(e) => setInfants(e.target.value)} 
+      placeholder=""
+      className="border rounded-md p-3 w-full" 
+    />
+  </div>
+        {
+        dataError && (
+        Object.entries(dataError).map(([key, value]) => (
+          <p key={key} className="bg-red-500 text-white text-center p-1 block rounded-md">Error: {value} {key}</p>
+        ))
+      )}
+
+  <div className="text-center">
+    <button 
+      onClick={fetchFlights} 
+      className="bg-blue-400 text-white font-bold py-2 px-4 mt-5 rounded-md hover:bg-blue-600 transition-all"
+      >
+      Search Flights
+    </button>
+  </div>
+         {loading && (
+          <div className="fixed inset-0 bg-black bg-opacity-50 flex flex-col justify-center items-center z-50">
+            <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 mb-2"></div>
+            <p className="text-white">Fetching Data...</p>
+          </div>
+    )}
+
+  <div>
+    {location.pathname === '/departure' && <Departure />}
+  </div>
+</div>
+
+  );
+};
+
+export default FlightSearch;
